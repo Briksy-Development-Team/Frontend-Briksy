f49cf2b75f405d42934d18c592d2938eb1a2bbbc
Siddharth0436 <86853383+Siddharth0436@users.noreply.github.com>
2026-04-07 11:54:34 +0530

    axios

--- admin/src/app/services/api/axiosInstance.ts
+++ admin/src/app/services/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {getAuth} from '../../modules/auth/core/AuthHelpers'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL || 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_APP_API_URL || 'https://briksy.elsolveit.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
